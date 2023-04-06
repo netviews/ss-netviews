c9130a635aab7c8a551e6d63e2b4f46b1b62b9a5
iahandy <iahandy@ncsu.edu>
2023-04-06 11:43:05 -0400

    Server class prints response messages

--- ONOS_Apps/nifwd_combined/app/src/main/java/org/onosproject/nifwd_combined/impl/Server.java
+++ ONOS_Apps/nifwd_combined/app/src/main/java/org/onosproject/nifwd_combined/impl/Server.java
@@ -1,23 +1,22 @@
 package org.onosproject.nifwd_combined.impl;
 
 import java.io.*;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 import gov.nist.csd.pm.exceptions.PMException;
 
-import org.slf4j.Logger;
-
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class Server {
 //    public static void main(String[] args) {
 //        connectToServer();
 //    }
+	
 
     public static void connectToServer() {
         //Try connect to the server on an unused port eg 9191. A successful connection will return a socket
+    	
         try(ServerSocket serverSocket = new ServerSocket(9191)) {
             Socket connectionSocket = serverSocket.accept();
 
@@ -26,21 +25,15 @@ public class Server {
             OutputStream outputFromServer = connectionSocket.getOutputStream();
 
             Scanner scanner = new Scanner(inputToServer, "UTF-8");
-//            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
-//
-//            serverPrintOut.println("Hello World! Enter Peace to exit.");
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
 
-            //Have the server take input from the client and echo it back
-            //This should be placed in a loop that listens for a terminator text e.g. bye
-//            boolean done = false;
+            
+            serverPrintOut.println("Server has been created on port 9191");
 
             while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-//                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
-//
-//                if(line.toLowerCase().trim().equals("peace")) {
-//                    done = true;
-//                }
+                serverPrintOut.println("You entered something");
+
                 if (line.equals("exit")) {
                 	break;
                 }
@@ -49,13 +42,13 @@ public class Server {
                 try {
 					PolicyEngine.getInstance().createPolicyGraph("/PATH-FROM-HOME/input-files/demo-topo-ref/topo-ref-policy.json");
 				} catch (PMException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					 serverPrintOut.println(e.getLocalizedMessage());
 				};
-//                policyEngine.createPolicyGraph("/PATH-FROM-HOME/input-files/demo-topo-ref/topo-ref-policy.json");
+				 serverPrintOut.println("Might have changed the policy engine");
             }
             
             serverSocket.close();
+            scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
